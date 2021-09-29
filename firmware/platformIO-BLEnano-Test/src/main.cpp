3bbf7a06f74ddcdada080fe0dcf6ce771bbd24e3
Dhruva Gole <goledhruva@gmail.com>
2021-09-29 18:18:33 +0530

    Tested basic tone example
    Also added a example of some song player, but yet to test it
    
    Signed-off-by: Dhruva Gole <goledhruva@gmail.com>

--- firmware/platformIO-BLEnano-Test/src/main.cpp
+++ firmware/platformIO-BLEnano-Test/src/main.cpp
@@ -1,9 +1,13 @@
 // Define ONLY one of these:
-#define DHT 1
+#define DHT 0
 #define MOTION 0
 #define SERIAL_ECHO 0
 #define IMU_MOTION_ACCEL 0
 #define IMU_MOTION_GYRO 0
+#define TONE 1
+#define SONG 0  // UNTESTED
+
+#include <Arduino.h>
 
 #if MOTION == 1
 /*
@@ -441,3 +445,124 @@ void loop()
   }
 }
 #endif // IMU_MOTION_GYRO
+
+#if TONE == 1
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+
+  // iterate over the notes of the melody:
+
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+
+    int noteDuration = 1000 / noteDurations[thisNote];
+
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+
+    // the note's duration + 30% seems to work well:
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    // stop the tone playing:
+
+    noTone(8);
+
+  }
+}
+
+void loop() {
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+
+    int noteDuration = 1000 / noteDurations[thisNote];
+
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+
+    // the note's duration + 30% seems to work well:
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    // stop the tone playing:
+
+    noTone(8);
+
+  }
+  // no need to repeat the melody.
+}
+
+#endif
+
+#if SONG == 1
+/* 
+This example demonstrates how to use the SDFAT library for audio playback.
+Read time and program space are reduced by using SDFAT directly
+
+Requirements:
+The SDFAT library must be installed. See http://code.google.com/p/sdfatlib/ 
+The line #define SDFAT MUST be uncommented in pcmConfig.h
+
+"error: 'File' has not been declared" means you need to read the above text
+*/
+
+#include <SdFat.h>
+SdFat sd;
+
+#define SD_ChipSelectPin 53  //use digital pin 4 on arduino Uno, nano etc, or can use other pins
+#include <TMRpcm.h>          //  also need to include this library...
+#include <SPI.h>
+
+TMRpcm audio;   // create an object for use in this sketch
+
+void setup(){
+
+  audio.speakerPin = 11; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+  pinMode(12,OUTPUT); //Pin pairs: 9,10 Mega: 5-2,6-7,11-12,46-45  
+  
+  Serial.begin(115200);
+  
+  if (!sd.begin(53, SPI_FULL_SPEED)) { return; 
+  }else{ Serial.println("SD OK"); }
+    
+  audio.play("song.wav");
+}
+
+void loop(){  
+
+  
+  if(Serial.available()){   
+    switch(Serial.read()){
+      case 'p': audio.play("song.wav"); break;
+      default: break;
+    }
+  }
+
+
+}
+#endif
